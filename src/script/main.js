5d640483e89f65fc8e24beec18808dcef809e503
Yoshifumi-T <2g.4423@gmail.com>
2020-09-05 22:22:23 +0900

    調整

--- src/script/main.js
+++ src/script/main.js
@@ -11,9 +11,8 @@ class Main {
         this.scene = null;
         this.camera = null;
         this.controls = null;
-        this.raycaster = new THREE.Raycaster();
         this.group = new THREE.Group();
-        this.vecrtor = { x: 0, y: 0, z: 0 }
+        this.vecrtor = { x: 0, y: 0, z: 0 };
         this.mode = 'control';
         this.canvas = document.querySelector('#canvas');
         this.click = new Object();
@@ -45,7 +44,7 @@ class Main {
         const renderStartdist = 1;
         const renderStopdist = 10000;
         this.camera = new THREE.PerspectiveCamera(angle, aspect, renderStartdist, renderStopdist);
-        this.camera.position.set(0, 0, +1000);
+        this.camera.position.set(800, 500, +1000);
         this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
 
         // 平行光源
@@ -83,14 +82,17 @@ class Main {
             this.meshList.push(mesh);
         }
 
-        this.meshList.forEach(e => this.scene.add(e));
-        this.scene.add(this.group);
+        for(const e of this.meshList) {
+            this.scene.attach(e);
+        }
+
+        this.scene.attach(this.group);
         this.scene.add(light);
+
+        this.tick();
 
         document.addEventListener('mousedown', this.mouseDown.bind(this));
         document.addEventListener('mouseup', this.mouseUp.bind(this));
-
-        this.tick();
     }
 
     tick() {
@@ -105,104 +107,9 @@ class Main {
 
         this.controls.enableRotate = false;
 
-        const mouse = new THREE.Vector2();
-        mouse.x =   (this.click.start.x / window.innerWidth) * 2 - 1;
-        mouse.y = - (this.click.start.y / window.innerHeight) * 2 + 1;
-
-        this.raycaster.setFromCamera(mouse, this.camera);
-        this.intersects = this.raycaster.intersectObjects(this.scene.children, true);
-
-        if(this.intersects.length < 1) {
-            this.renderer.render(this.scene, this.camera);
-            return;
-        }
-
-        if(this.group.children.length === 0) {
-            this.group = new THREE.Group();
-            this.scene.add(this.group);
-
-            const point = this.intersects[0].point;
-            // console.log(point)
-
-            for(const e of this.meshList) {
-                const vec = new THREE.Vector3();
-                const pos = e.getWorldPosition(vec)
-
-                // 立方体の中心とサイズ(半分)から最小値と最大値を計算
-                const minX = (pos.x - 50);
-                const maxX = (pos.x + 50);
-
-                const minY = (pos.y - 50);
-                const maxY = (pos.y + 50);
-
-                const minZ = (pos.z - 50);
-                const maxZ = (pos.z + 50);
-                
-                if(this.vecrtor.x !== 0) {
-                    // クリック座標が立方体に含まれる時、グループに追加する
-                    if(Math.abs(point.z) > 145) {
-                        if(point.y > minY && point.y < maxY) {
-                            this.rotation = 'x'
-                            this.group.add(e);
-                        }
-                    } else {
-                        if(Math.abs(point.x) > 145) {
-                            if(point.y > minY && point.y < maxY) {
-                                this.rotation = 'x'
-                                this.group.add(e);
-                            }
-                        }
-                        if(Math.abs(point.y) > 145) {
-                            if(point.z > minZ && point.z < maxZ) {
-                                this.rotation = 'xz'
-                                this.group.add(e);
-                            }
-                        }
-                    }
-                }
-
-                if(this.vecrtor.y !== 0) {
-                    // クリック座標が立方体に含まれる時、グループに追加する
-                    if(Math.abs(point.z) > 145) {
-                        if(point.x > minX && point.x < maxX) {
-                            this.rotation = 'y'
-                            this.group.add(e);
-                        }
-                    } else {
-                        if(Math.abs(point.y) > 145) {
-                            if(point.x > minX && point.x < maxX) {
-                                this.rotation = 'y'
-                                this.group.add(e);
-                            }
-                        }
-                        if(Math.abs(point.x) > 145) {
-                            if(point.z > minZ && point.z < maxZ) {
-                                this.rotation = 'yz'
-                                this.group.add(e);
-                            }
-                        }
-                    }
-                }
-            }
-
-            // TODO: カメラの角度によって回転の方向が変わる問題に対応する
-            if(this.rotation === 'x') {
-                this.group.rotation.y = this.vecrtor.x * 0.02;
-            } else if(this.rotation === 'y') {
-                this.group.rotation.x = this.vecrtor.y * 0.02;
-            } else if(this.rotation === 'xz') {
-                const direction = point.y > 0 ? -1 : 1
-                this.group.rotation.z = this.vecrtor.x * 0.02 * direction;
-            } else if(this.rotation === 'yz') {
-                const direction = point.x > 0 ? -1 : 1
-                this.group.rotation.z = this.vecrtor.y * 0.02  * direction;
-            }
-        }
-
         this.renderer.render(this.scene, this.camera);
     }
-
-
+    
     mouseDown(event) {
         document.addEventListener('mousemove', this.mouseMoveCallback, false);
 
@@ -218,63 +125,172 @@ class Main {
         
         const moveX = this.click.current.x - this.click.start.x;
         const moveY = this.click.current.y - this.click.start.y;
+        
+        this.vecrtor.x = moveX;
+        this.vecrtor.y = moveY;
 
-        if(Math.abs(moveX) > 30 && this.vecrtor.y === 0) {
-            this.vecrtor = { x: moveX, y: 0, z: 0 }
-            return;
-        }
-
-        if(Math.abs(moveY) > 30 && this.vecrtor.x === 0) {
-            this.vecrtor = { x: 0, y: moveY, z: 0 }
-            return;
-        }
+        this.createGroup();
     }
 
     mouseUp() {
         document.removeEventListener('mousemove', this.mouseMoveCallback, false);
 
-        // TODO: カメラの角度によって回転の方向が変わる問題に対応する
-        if (this.rotation === 'x' &&  this.vecrtor.x !== 0) {
-            let direction = this.vecrtor.x > 0 ? 1 : -1;
-            this.group.rotation.y = Math.PI / 2 * direction;
-        }
-
-        if( this.rotation === 'y' && this.vecrtor.y !== 0) {
-            let direction = this.vecrtor.y > 0 ? 1 : -1;
-            direction *= this.camera.position.z > 0 ? 1 : -1;
-            this.group.rotation.x = Math.PI / 2 * direction;
-        }
-
-        if(this.rotation === 'xz' && this.vecrtor.x !== 0) {
-            let direction = this.vecrtor.x < 0 ? 1 : -1;
-            direction *= this.camera.position.z > 0 ? 1 : -1;
-            direction *= this.camera.position.y > 0 ? 1 : -1;
-            this.group.rotation.z = Math.PI / 2 * direction;
-        }
-
-        if(this.rotation === 'yz' && this.vecrtor.y !== 0) {
-            let direction = this.vecrtor.y < 0 ? 1 : -1;
-            direction *= this.camera.position.x > 0 ? 1 : -1;
-            this.group.rotation.z = Math.PI / 2 * direction;
-        }
-
-        this.rotation = ''
-        this.renderer.render(this.scene, this.camera);
-
-        for(const e of this.meshList) {
-            e.matrixAutoUpdate = false;
-            e.matrix.fromArray(e.matrixWorld.elements);
-            this.scene.add(e);
-        }
-
-        this.scene.remove(this.group);
-        this.vecrtor = { x: 0, y: 0, z: 0 }
-
-        this.renderer.render(this.scene, this.camera);
-
         this.click.start.x = 0;
         this.click.start.y = 0;
         this.click.current.x = 0;
         this.click.current.y = 0;
+
+        if(this.group.children.length !== 9) {
+            for(const e of this.meshList) {
+                this.scene.attach(e);
+            }
+
+            return;
+        }
+
+        this.decideRotationDirection();
+        this.executeRotate();
     }
+
+    createGroup() {
+        const mouse = new THREE.Vector2();
+        mouse.x =   (this.click.current.x / window.innerWidth ) * 2 - 1;
+        mouse.y = - (this.click.current.y / window.innerHeight) * 2 + 1;
+
+        const ray1 = new THREE.Raycaster();
+        ray1.setFromCamera(mouse, this.camera);
+        const intersects1 = ray1.intersectObjects(this.scene.children, true);
+
+        if(intersects1.length === 0) return;
+
+        const object = intersects1[0].object;
+        const point = intersects1[0].point.clone();
+
+        // クリックした面の法線ベクトルを取得する
+        const face  = intersects1[0].face.normal.clone();
+        
+        // オブジェクトに合わせて回転させる
+        face.applyQuaternion(object.quaternion);
+
+        const ray = new THREE.Raycaster();
+        ray.set(point, face.negate());
+        const intersects = ray.intersectObjects(this.scene.children, true);
+
+        if(intersects.length === 0) return;
+
+        for(const e of intersects) {
+            if(this.group.children.includes(e.object) === true) continue;
+            this.group.attach(e.object)
+        }
+    }
+
+    decideRotationDirection() {
+        const base = this.group.children[0].position;
+        
+        const checkX = [];
+        const checkY = [];
+        const checkZ = [];
+
+        for(const mesh of this.group.children) {
+            checkX.push(Math.abs(base.x - mesh.position.x) < 5);
+            checkY.push(Math.abs(base.y - mesh.position.y) < 5);
+            checkZ.push(Math.abs(base.z - mesh.position.z) < 5);
+        }
+
+        if(checkX.includes(false) === false) {
+            return this.rotationDirection = 'y';
+        }
+
+        if(checkY.includes(false) === false) {
+            return this.rotationDirection = 'x';
+        }
+
+        if(checkZ.includes(false) === false) {
+            return this.rotationDirection = 'z';
+        }
+    }
+
+    executeRotate() {
+        const vector = new THREE.Vector3(this.vecrtor.x, this.vecrtor.y, 0);
+        
+        if(this.rotationDirection === 'x') {
+            let direction = vector.x > 0 ? 1 : -1;
+            this.rotateX(Math.PI / 2, direction)
+        }
+
+        if(this.rotationDirection === 'y') {
+            let direction = vector.y > 0 ? 1 : -1;
+            direction *= this.camera.position.z > 0 ? 1: -1
+            this.rotateY(Math.PI / 2, direction);
+        }
+        
+        if(this.rotationDirection === 'z') {
+            let direction;
+            
+            if(Math.abs(vector.x) > 50) {
+                direction = vector.x < 0 ? 1 : -1;
+                direction *= this.camera.position.y > 0 ? 1: -1;
+            } 
+
+            if(Math.abs(vector.y) > 50) {
+                direction = vector.y < 0 ? 1 : -1;
+                direction *= this.camera.position.x > 0 ? 1: -1;
+            }
+            
+            this.rotateZ(Math.PI / 2, direction);
+        }
+    }
+
+    rotateX(angle, direction) {
+        if( Math.abs(this.group.rotation.y) >= Math.abs(angle) ) {
+            this.group.rotation.y = angle * direction;
+            this.renderer.render(this.scene, this.camera);
+            
+            this.rotateFinish(); 
+            
+            return cancelAnimationFrame(this.id);
+        }
+
+        this.group.rotation.y += 6 * 0.02 * direction;
+
+        this.id = requestAnimationFrame(this.rotateX.bind(this, angle, direction));
+    }
+
+    rotateY(angle, direction) {
+        if( Math.abs(this.group.rotation.x) >= Math.abs(angle) ) {
+            this.group.rotation.x = angle * direction;
+            this.renderer.render(this.scene, this.camera);
+
+            this.rotateFinish(); 
+            return cancelAnimationFrame( this.id );
+        }
+
+        this.group.rotation.x += 6 * 0.02 * direction;
+
+        this.id = requestAnimationFrame(this.rotateY.bind(this, angle, direction));
+    }
+
+    rotateZ(angle, direction) {
+        if( Math.abs(this.group.rotation.z) >= Math.abs(angle) ) {
+            this.group.rotation.z = angle * direction;
+            this.renderer.render(this.scene, this.camera);
+            
+            this.rotateFinish(); 
+
+            return cancelAnimationFrame( this.id );
+        }
+
+        this.group.rotation.z += 6 * 0.02 * direction;
+
+        this.id = requestAnimationFrame(this.rotateZ.bind(this, angle, direction));
+    }
+
+    rotateFinish() {
+        for(const e of this.meshList) {
+            this.scene.attach(e);
+        }
+        this.group.rotation.set(0, 0, 0);
+        this.rotationDirection = null;
+    }
+
 }
